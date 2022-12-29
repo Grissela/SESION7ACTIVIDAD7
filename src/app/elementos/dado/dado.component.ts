c26966b6ea27e5dcfb69fe98901dc61fee576cbb
Grissela <108595899+Grissela@users.noreply.github.com>
2022-12-29 11:59:19 -0500

    Condicionando que sea del 1 al 6

--- src/app/elementos/dado/dado.component.ts
+++ src/app/elementos/dado/dado.component.ts
@@ -9,6 +9,17 @@ export class DadoComponent implements OnInit {
 
   constructor() { }
 
+  min:number=1;
+  max:number=6;
+
+  random!:number;
+  aleatorio(){
+    //para que sea del 1 al 6, con un maximo de 5 para que agregue 1
+    this.random = Math.round(Math.random()*5)+1;
+    
+    // this.random = Math.round(Math.random()*(((this.max - this.min + this.min) + this.min)));/*Valor maximo y valor minimo el minimo le agregue 1*/ 
+
+  }
   ngOnInit(): void {
   }
 

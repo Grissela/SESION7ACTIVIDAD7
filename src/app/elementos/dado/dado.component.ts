f7f28c64c04bd70e45706e1db5022bc8a633033f
Grissela <108595899+Grissela@users.noreply.github.com>
2022-12-29 13:45:44 -0500

    Diseño y animacion

--- src/app/elementos/dado/dado.component.ts
+++ src/app/elementos/dado/dado.component.ts
@@ -8,18 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class DadoComponent implements OnInit {
 
   constructor() { }
-
-  min:number=1;
-  max:number=6;
-
+  
   random!:number;
+  
+  
   aleatorio(){
     //para que sea del 1 al 6, con un maximo de 5 para que agregue 1
-    this.random = Math.round(Math.random()*5)+1;
-    
+     this.random = Math.round(Math.random()*5)+1;
+
+    // this.random = Math.round(Math.random() * (6 - 1) + 1);
+    // for(let i=0; i<this.usados.length; i++){
+    //  this.random= this.usados[i]
+    //  console.log(this.random)
+    // var num = [1,2,3,4,5,6];
+    // this.random = Math.ceil(Math.random()*5);
+    // var number = num[this.random];
+    // }
     // this.random = Math.round(Math.random()*(((this.max - this.min + this.min) + this.min)));/*Valor maximo y valor minimo el minimo le agregue 1*/ 
 
   }
+  
   ngOnInit(): void {
   }
 
